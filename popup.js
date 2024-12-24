2d0867c949e03e431e57f13409283de3a970ac64
chlwnsgurg <chlwnsgurg@gmail.com>
2024-12-24 17:49:26 +0900

    storage to save state: migrated from web storage API to extension storage API

--- popup.js
+++ popup.js
@@ -2,22 +2,26 @@ const toggle = document.getElementById('toggle');
 const state = document.getElementById('state');
 const description = document.getElementById('description');
 
-let savedChecked = localStorage.getItem('checked') === 'true';
-toggle.checked = savedChecked;
-(set_popup = (checked) => {
-    if (checked) {
+function set_popup(filter) {
+    if (filter) {
         state.innerText = 'Filtered';
         description.innerHTML = 'You are <span style="color: #2196F3;">safe</span> in Matrix';
     } else {
         state.innerText = 'Unfiltered';
         description.innerHTML = 'You are exposed to <span style="color: red;">danger</span>';
     }
-})(savedChecked);
+}
 
+document.addEventListener('DOMContentLoaded', async () => {
+    const result = await chrome.storage.local.get("filter");
+    let filter = result.filter;
+    toggle.checked = filter;
+    set_popup(filter);
+});
 
 toggle.addEventListener('change', (event) => {
-    let checked=event.target.checked;
-    localStorage.setItem('checked', checked);
-    set_popup(checked);
+    let filter=event.target.checked;
+    chrome.storage.local.set({filter:filter});
+    set_popup(filter);
 });
 
--- scripts/content.js
+++ scripts/content.js
@@ -1,11 +1,10 @@
+let comments=document.querySelectorAll('ytd-comment-thread-renderer');
 
-document.querySelectorAll('ytd-comment-thread-renderer');
 // if comments exist
 if (comments) {
     for (let comment of comments) {
         let text=comment.querySelector("#content-text").innerText; //text is a string 
-        let words=text.split(" ");
         /* Filter Comments */
-        comment.querySelector("#content-text").innerText = "WANG!~";
+        comment.style.visibility = 'hidden';
     }
 }
